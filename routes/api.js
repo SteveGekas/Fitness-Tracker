821d30badbd172027c9e16d8dc744f2306b88e22
Steve Gekas <75593974+SteveGekas@users.noreply.github.com>
2021-04-24 20:13:22 -0400

    created api routes in routes folder

---
+++ routes/api.js
@@ -0,0 +1,72 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+//creates new workout route
+router.post("/workouts", ({ body }, res) => {
+    Workout.create({})
+        .then(WorkoutDB => {
+            console.log(`post: ${WorkoutDB}`)
+            res.json(WorkoutDB);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+//finds all workouts in db route
+router.get("/workouts", (req, res) => {
+   //aggregate function
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: { $sum: "$exercises.duration" }
+            }
+        }
+    ])
+        .then(WorkoutDB => {
+            console.log(`get: ${WorkoutDB}`);
+            res.json(WorkoutDB);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+//adds a workout route
+router.put("/workouts/:id", ({ params, body }, res) => {
+
+    Workout.findOneAndUpdate(
+        { _id: params.id },
+        { $push: { exercises: body } },
+        { new: true })
+        .then(WorkoutDB => {
+            console.log(`put: ${WorkoutDB}`)
+            res.json(WorkoutDB);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+})
+
+//retrieves last 7 workouts for stats page route
+router.get("/workouts/range", (req, res) => {
+   //aggregate function
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: { $sum: "$exercises.duration" },
+                totalWeight: { $sum: "$exercises.weight" }
+            }
+        }
+    ])
+        .limit(7)
+        .then(WorkoutDB => {
+            console.log(`range: ${WorkoutDB}`)
+            res.json(WorkoutDB);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        })
+})
+
+module.exports = router;
